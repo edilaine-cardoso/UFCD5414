b791294bf0d76a74596e97b030437129a77db803
Edilaine Cardoso <edilaine.cardoso.dev@gmail.com>
2025-06-25 10:38:36 +0100

    produtos

--- produto.php
+++ produto.php
@@ -5,8 +5,36 @@
     include("cabecalho.php");
    ?>
 <!--aqui fica o conteudo--> 
+<div class="produto">
+  <h2> Fuzzy cardigan   </h2>
+ <p>por apenas R$ 129,00</p>
+  <form action="">
+    <fieldset class="cores">
 
+      <legend>Escolha a cor: </legend>
 
+      <input type="radio" name="cor" value="verde" checked>
+      <label for="verde">
+          <img src="img/produtos/foto2-verde.png" alt="verde">
+      </label>
+    
+
+       <input type="radio" name="cor" value="rosa">
+      <label for="rosa">
+          <img src="img/produtos/foto2-rosa.png" alt="rosa">
+      </label>
+
+       <input type="radio" name="cor" value="azul" >
+      <label for="azul">
+          <img src="img/produtos/foto2-azul.png" alt="azul">
+      </label>
+
+  <input type="subimit" >
+  
+    </fieldset>
+  </form>
+
+</div>
 
 <?php include("rodape.php")?>
 
